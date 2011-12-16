83e9e621a3d90659b6e722e2446ea10db596936d
Remi <rd@r-2b.net>
2011-12-16 18:18:51 +0100

    Ajout pyvision

---
+++ pyvision/src/client.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from ZSI import ServiceProxy
+import sys
+
+MESSAGE = "Hello from Python!"
+
+def main():
+    server = ServiceProxy.ServiceProxy('./wsdl/binding.wsdl', use_wsdl=True)
+
+    print ' Sending: %s' % MESSAGE
+    response = server.echo(Message=MESSAGE)
+    print 'Response: %s' % response['Message']
+    one=1
+    two=2
+    response = server.add(One=one,Two=two)
+    print 'Response: %s' % response['Result']
+
+
+if __name__ == '__main__':
+    main()
---
+++ pyvision/src/server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from ZSI import dispatch
+from PyVision_server import EchoResponse
+from PyVision_server import AddResponse
+
+def echo(message):
+    response = EchoResponse()
+    response._Message = message
+    return response
+
+def add( operators ):
+    response = AddResponse()
+    print operators
+    response._Result = 0
+    for o in operators:
+        op = operators[o]
+        response._Result += op
+    return response
+
+if __name__ == '__main__':
+    dispatch.AsServer(port=8080)
