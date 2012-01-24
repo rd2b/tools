194ee7e271062fb1811a2f91ea400b63413dc587
Remi <rd@r-2b.net>
2012-01-24 19:10:42 +0100

    Mise en place de tests unitaires

--- pyvision/src/ComplexTypes.py
+++ pyvision/src/ComplexTypes.py
@@ -1,4 +1,5 @@
 
+from datetime import datetime, timedelta
 
 class Alert(object):
     id = None
@@ -21,18 +22,29 @@ class Alert(object):
                self.message, str(self.priority))
 
 class Control(object):
-    def __init__(self, starting, seconds, command):
+    def __init__(self, starting = datetime.now(), 
+                       seconds = 0, 
+                       command = ""):
        self.starting = starting
        self.seconds = seconds
-       self.command = commands
+       self.command = command
     
     def schedule(self):
+        """
+            Return next scheduling for Control
+            Return None if Control should not be schedule
+        """
+        now = datetime.now()
+        if self.seconds < 0 or ( self.seconds == 0 \
+		and ( not self.starting or self.starting < now )):
+            return None 
         if not self.starting:
-            self.starting = datetime.now()
-        nextexecution = c.starting
-        if not self.seconds > 0 :
+            self.starting = now
+        nextexecution = self.starting
+        if self.seconds and self.seconds > 0 :
              delta = timedelta(seconds = self.seconds)
-             while nextexecution < datetime.now() and delta:
+             while nextexecution < now and delta:
                  nextexecution = nextexecution + delta
+                 print nextexecution
         return nextexecution
 
