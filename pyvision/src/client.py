b7fa54f105bfd919cbe78bb9bb74253e53d55097
Remi <rd@r-2b.net>
2012-02-02 09:08:25 +0100

    Amelioration du client avec pylint

--- pyvision/src/client.py
+++ pyvision/src/client.py
@@ -1,45 +1,47 @@
 #!/usr/bin/env python
 #########################################################
 #	Name: 	client.py				#
-#	Description: TODO				#
+#	Description: client used to check 		#
+#			and send acontrols		#
 #########################################################
 
 
 from ZSI import ServiceProxy
 from ComplexTypes import Control, Alert
 
-import sys
+import time
 import logging
 import argparse
 import ConfigParser
+import shlex
+import os
+import subprocess
 
-from datetime import datetime, timedelta
-import time
-from collections import deque
+from datetime import datetime 
+from multiprocessing import Process
 
 MESSAGE = "Hello from Python!"
 DEFAULTCONFIGFILE = "/etc/pyvision/configfile"
 
-def getCommands(path=None, extension=".pv"):
-    import os
+def getcommands(path=None, extension=".pv"):
     if not path or not os.path.isdir(path):
         return None
     entries = os.listdir(path)
-    logging.debug("Entries in %s:%s"%(path,entries))
+    logging.debug("Entries in "+ path +","+ str(entries))
     commands = []
-    for e in entries:
-        myfile = path + os.sep + e
+    for entry in entries:
+        myfile = path + os.sep + entry
         if os.path.splitext(myfile)[1] == extension :
-            command = Control(path=myfile)
+            command = Control(path = myfile)
             if command :
                 commands.append(command)
         else :
-            logging.info("%s is not a valid configfile"% myfile)
+            logging.info(str(myfile) + " is not a valid configfile")
 
     logging.debug("Ordering list for execution")
-    logging.debug("Before :%s"% commands)
+    logging.debug("Before :"+ str(commands))
     commands.sort()
-    logging.debug("After :%s"% commands)
+    logging.debug("After :"+ str(commands))
     
     return commands
     
@@ -50,19 +52,20 @@ def execute(control):
     if not command:
         return None
     
-    import shlex, subprocess
     args = shlex.split(command)
-    logging.info("Excuting %s:"%command)
-    logging.debug("Arguments %s:"%args)
-    p = subprocess.Popen(args,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    returncode = p.wait()
-    output = p.stdout.read() + p.stderr.read()
-    logging.info("Result : %s"%output)
+    logging.info("Excuting "+ command)
+    logging.debug("Arguments "+ str(args))
+    process = subprocess.Popen(args, 
+                               stdout=subprocess.PIPE, 
+                               stderr=subprocess.PIPE)
+    returncode = process.wait()
+    output = process.stdout.read() + process.stderr.read()
+    logging.info("Result : "+str(output))
     return (output, returncode)
     
-def convertToAlert(control, result):
+def converttoalert(control, result):
     if not control or not result:
-        return none
+        return None
     alert = Alert(date = str(datetime.now()),
           sender = "",
           reference = "",
@@ -73,8 +76,7 @@ def convertToAlert(control, result):
 
 def send(alert):
     if not alert:
-         return None
-    nsdict = { 'types' : 'http://pycon.org/typs' }
+        return None
     server = ServiceProxy.ServiceProxy( './wsdl/binding.wsdl')
     response = server.send( Date = alert.date,
                             Sender = alert.sender,
@@ -82,28 +84,28 @@ def send(alert):
                             Host = alert.host,
                             Message = alert.message,
                             Priority = alert.priority)
-    logging.info("Got response : %s"% response)
+    logging.info("Got response : "+ str( response))
 
 def run(mycontrols = None):
-    def runControl(myc = None):
-        logging.info("Initializing new control: %s"%myc)
+    def runcontrol(myc = None):
+        logging.info("Initializing new control: "+ str(myc))
         if not myc:
             return None
-        logging.info("Initializing new control: %s"%myc)
+        logging.info("Initializing new control: "+ str(myc))
         while True:
-           schd = myc.schedule()
-           dt = schd - datetime.now()
-           time.sleep(dt.total_seconds())
-           logging.info("Executing new control: %s"%myc)
-           result = execute(myc)
-           logging.debug("Result = '%s', Code = '%s' "%(result[0],result[1]))
-           send(convertToAlert(control = myc, result = result))
+            schd = myc.schedule()
+            delta = schd - datetime.now()
+            time.sleep(delta.total_seconds())
+            logging.info("Executing new control: "+ str(myc))
+            result = execute(myc)
+            logging.debug("Result = '"+str(result[0])+
+                          "', Code = '"+str(result[1])+"'")
+            send(converttoalert(control = myc, result = result))
            
    
-    from multiprocessing import Process
-    for c in mycontrols:
-        p = Process(target=runControl, args=(c,))
-        p.start()
+    for control in mycontrols:
+        process = Process(target=runcontrol, args=(control,))
+        process.start()
 
 
 def main():
@@ -118,43 +120,40 @@ def main():
         configfile = DEFAULTCONFIGFILE 
     else:
         configfile = args.configfile 
-    logging.info("Using configfile %s"%configfile)
+    logging.info("Using configfile " + str(configfile))
 
     try:
         section = "PyVision"
         configparser = ConfigParser.ConfigParser()
         configparser.read(configfile)
         logging.debug(str(configparser.sections()))
-        commandsdir = configparser.get(section,"commandsdir")
-        extension = configparser.get(section,"extension")
+        commandsdir = configparser.get(section, "commandsdir")
+        extension = configparser.get(section, "extension")
     except ConfigParser.NoSectionError as nse:
         logging.error(nse)
         return 401
     logging.info("Using commands files in %s with exetnsion %s" \
 			% (commandsdir, extension))
 
-    run(getCommands(commandsdir, extension))
-    return 0
+    run(getcommands(commandsdir, extension))
 
-    nsdict = { 'types' : 'http://pycon.org/typs' }
-    server = ServiceProxy.ServiceProxy( './wsdl/binding.wsdl')
-
-    print ' Sending: %s' % MESSAGE
-    response = server.echo(Message = MESSAGE)
-    print 'Response: %s' % response['Message']
-    one=1
-    two=2
-    response = server.add(One=one,Two=two)
-    print 'Response: %s' % response['Result']
-    response = server.send( Date = '2011/12/17',
-                            Sender = 'tekos@test.net',
-                            Reference = 'disk',
-                            Host = "ref01.prod.test.net",
-                            Message = 'Free space on /var/log < 20% ',
-                            Priority = 0)
-    print response
-    response = server.getalerts(Message = "Gimme")
-    print response
+#
+#    print ' Sending: %s' % MESSAGE
+#    response = server.echo(Message = MESSAGE)
+#    print 'Response: %s' % response['Message']
+#    one=1
+#    two=2
+#    response = server.add(One=one,Two=two)
+#    print 'Response: %s' % response['Result']
+#    response = server.send( Date = '2011/12/17',
+#                            Sender = 'tekos@test.net',
+#                            Reference = 'disk',
+#                            Host = "ref01.prod.test.net",
+#                            Message = 'Free space on /var/log < 20% ',
+#                            Priority = 0)
+#    print response
+#    response = server.getalerts(Message = "Gimme")
+#    print response
 
 
 if __name__ == '__main__':
