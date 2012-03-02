d697770d7bee7459ffffd3a71aedc6f0658e7bb0
Remi <rd@r-2b.net>
2012-03-02 16:24:27 +0100

    Helloworld en c

---
+++ cplusplus/HelloWorld/test.c
@@ -0,0 +1,10 @@
+//########################################################
+//#	Name: 	test.c		#
+//#	Description: TODO	#
+//#########################################################
+#include <stdio.h>
+
+int main( int argc, const char* argv[] ){
+    printf("Hello World\n");
+}
+
