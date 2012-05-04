30853cc7de9134c0324a7661b11776d6101d4d7d
Remi <rd@r-2b.net>
2012-05-04 14:42:05 +0200

    diverses ameliorations pour la gestion de la creation des vm

--- vmscripts/scripts/boot.sh
+++ vmscripts/scripts/boot.sh
@@ -8,8 +8,8 @@ sed -i "s/VCS=\"bzr\"/VCS=\"git\"/" /etc/etckeeper/etckeeper.conf
 etckeeper init
 
 apt-get install -qqy --force-yes glusterfs-client
-sed -i "s/option\ remote-host\ 127.0.0.1/option\ remote-host\ 192\.168\.122\.101/" /etc/glusterfs/glusterfs.vol
 mkdir -p /data/commun
 
 echo "# Partage glusterfs :" >> /etc/fstab
-echo "/etc/glusterfs/glusterfs.vol /data/commun  glusterfs  defaults  0  0" >> /etc/fstab
+echo "vm01.acgform.local:/COMMUN /data/commun glusterfs defaults 0 0" >> /etc/fstab
+
