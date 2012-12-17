2f4ab343f76bcd7e81da47e8ed02c1e4aac0577f
Remi <rd@r-2b.net>
2012-12-17 14:15:39 +0100

    Gestion de plusieurs mots clés

--- webcheck/webcheck.py
+++ webcheck/webcheck.py
@@ -11,6 +11,72 @@ import urllib2
 
 import cherrypy
 
+class UrlCheck():
+    def __init__(self, url, timeout = 2):
+        self.url = url
+        self.timeout = timeout
+        self.keywords = dict()
+        self.available = False
+
+    def addkeyword(self, keyword):
+        self.keywords[keyword] = False
+
+    def finded(self, keyword):
+        self.keywords[keyword] = True
+
+    def missing(self, keyword):
+        self.keywords[keyword] = False
+
+    def __str__(self):
+        return str({ 
+            url : self.url, 
+            available : str(self.available),
+            timeout : str(self.timeout),
+            keywords : str(self.keywords)
+            })
+
+    def markavailable(self):
+        self.available = True
+
+    def markunavailable(self):
+        self.available = False
+
+    def runcheck(self):
+        print "Checking " + self.url
+        try:
+            openned = urllib2.urlopen( self.url, timeout = self.timeout )
+            self.markavailable()
+            content = openned.read();
+
+            for keyword in self.keywords.keys():
+                print "Checking " + self.url + " with keyword " +keyword
+                if keyword in content:
+                    self.finded(keyword)
+                else:
+                    self.missing(keyword)
+        except ValueError:
+            print "Not a valid URL " + url
+        except IOError:
+            self.markunavailable()
+
+    def repport (self):
+        message = ""
+        if self.available:
+            message = "{0} available.".format(self.url)
+        else:
+            message = "{0} not available.".format(self.url)
+
+        for keyword in self.keywords.keys():
+            print self.keywords
+            if self.keywords[keyword]:
+                message += "{0} found with correct keyword '{1}'.".format(
+                        self.url, keyword)
+            else:
+                message += "{0} has no keyword like '{1}'.".format(
+                        self.url, keyword)
+        return message
+ 
+
 class Checker():
     """ Checks if keywords are in given urls """
 
@@ -18,26 +84,17 @@ class Checker():
         self.urls = {}
         self.keywords = {}
         self.times = {}
-        self.times["timeout"] = 120
 
     def add(self, url):
-        if not self.urls.has_key(url):
-            self.urls[url] = False
+        if ( url not in self.urls and not url == "" ):
+            self.urls[url] = UrlCheck(url)
 
     def addkeyword(self, url, keyword):
-        if not self.urls.has_key(url):
-            self.add(url)
-        self.keywords[url] = keyword
-
-    def markavailable(self, url):
-        self.urls[url] = True
-
-    def markunavailable(self, url):
-        self.urls[url] = False
+        self.add(url)
+        self.urls[url].addkeyword(keyword)
 
     def __str__(self):
         urls = str(self.urls)
-        keywords = str(self.keywords)
         return str((urls, keywords))
 
     def tohtml(self):
@@ -48,44 +105,22 @@ class Checker():
     def schedulenext(self):
         self.times["timeout"] = 120
 
-
     def report(self, html = False):
         response = "" 
         for url in self.urls.keys():
-            message = ""
-            if (self.urls[url] and self.keywords.has_key(url)):
-                message = \
-                        "{0} found with correct keyword '{1}'.".format(
-                                url, self.keywords[url])
-            elif (self.urls[url]):
-                message = "{0} found.".format(url)
-            else:
-                message = "{0} not found.".format(url)
-            response += message
+            response += self.urls[url].repport()
             if html:
                 response += "<br>"
         return response
 
+    def check(self, url):
+       if url in self.urls:
+           self.urls[url].runcheck()
 
-    def runcheck(self, url):
-        print "Checking " + url
-        try:
-            openned = urllib2.urlopen( url, timeout = 2 )
-            if self.keywords.has_key(url):
-                if self.keywords[url] in openned.read():
-                    self.markavailable(url)
-                else:
-                    self.markunavailable(url)
-            else:
-                self.markavailable(url)
-        except ValueError:
-            print "Not a valid URL " + url
-        except IOError:
-            self.markunavailable(url)
-
-    def check(self ):
-        for url in self.urls.keys():
-            self.runcheck(url)
+    def checkall(self ):
+        for url in self.urls.values():
+            print url
+            url.runcheck()
 
 class WebServer:
     def __init__(self, checker):
@@ -93,14 +128,12 @@ class WebServer:
 
     @cherrypy.expose
     def index(self, url = None, keyword = None):
-        if( url and keyword ):
-            self.checker.addkeyword(url, keyword)
-            self.checker.runcheck(url)
-        elif ( url ):
+        if url:
             self.checker.add(url)
-            self.checker.runcheck(url)
+            if keyword:
+                self.checker.addkeyword(url, keyword)
 
-        self.checker.check()
+            self.checker.check(url)
 
         formulaire = """
             <form action="index">
@@ -119,19 +152,24 @@ def main():
     parser = argparse.ArgumentParser(description="Checks if urls are fine")
     parser.add_argument(
         '-u', '--url', 
-        type=str, default=[], action='append', help='Url to check')
+        type=str, default='', action='append', help='Url to check')
     parser.add_argument(
-        '-U', '--urls', type=str, default=[], help='Url to check' )
+        '-U', '--urls', type=str, default="", help='Url to check' )
     args = parser.parse_args()
 
     checker = Checker()
+     
+    urls = []
+    urls.append(args.url)
+    urls.extend(args.urls.split())
 
-    for url in args.url + args.urls.split():
+    for url in urls:
         arr = url.split(",")
         checker.add(arr[0])
         if (len(arr) == 2):
             checker.addkeyword(arr[0], arr[1])
 
+    checker.checkall()
     cherrypy.quickstart(WebServer(checker))
 
 
